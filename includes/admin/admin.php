926e676e0df26b2d266a9417a23060e5f7485e57
Tushar Imran <tusharimran9909@gmail.com>
2025-03-01 19:36:42 +0600

    Added form basic

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -1,15 +1,36 @@
 <?php 
 
-add_action('kz_admin_dashboard', function(){
-    $menu_items = apply_filters(
-        'kz_admin_sub_menu_items', 
-        ['Menu one', 'Menu Two', 'Menu Three', 'Menu Four', 'Menu Five'],
-        'dashboard',
-        'another'
-    );
+// nonce verify
+// User role / capability check
 
-    foreach( $menu_items as  $menu_item){
-        echo '<li>' . $menu_item . '</li>';
+// sanitize
+// data validation - optional
+
+// escaping
+
+add_action('admin_post_ka_user_register', function(){
+    if(!isset($_POST['security']) || !wp_verify_nonce( $_POST['security'], 'ka_register_user')){
+        wp_die(esc_html__('Security Failed!', 'kodezen-academy'));
     }
-    
+
+    if(! current_user_can('manage_options')){
+        wp_die('Sorry, You have no permission to crate new user.');
+    }
+
+    $name = sanitize_text_field($_POST['name']);
+    $email = sanitize_email($_POST['email']);
+
+    if(!is_email($email)){
+        wp_die('Sorry, invalid email');
+    }
+
+    if(empty($name)){
+        wp_die('Sorry, name is empty');
+    }
+
+
+
+    var_dump($_POST);
+
+
 });
--- includes/admin/partials/dashboard.php
+++ includes/admin/partials/dashboard.php
@@ -1,7 +1,15 @@
 <h1>Welcome to Kodezen Academy</h1>
 
-<ul>
+<form method="post" action="<?php echo esc_url(admin_url('admin-post.php')) ?>">
+    <input type="hidden" name="action" value="ka_user_register" />
     <?php 
-        do_action('kz_admin_dashboard');
+        wp_nonce_field('ka_register_user', 'security');
     ?>
-</ul>
+    <div>
+        <input type="text" name="name" placeholder="<?php echo esc_attr__('Enter Your Name', 'kodezen-academy'); ?>" />
+    </div>
+    <div>
+        <input type="email" name="email" placeholder="Enter Your Email" />
+    </div>
+    <button type="submit">Submit</button>
+</form>
